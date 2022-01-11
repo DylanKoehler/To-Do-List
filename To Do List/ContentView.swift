ab07aebbaeb09a90790fdbad893c245a9804680b
Dylan Koehler <Dylankoehler3212@gmail.com>
2022-01-11 10:56:35 -0600

    created initial to do list.

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -2,15 +2,30 @@
 //  ContentView.swift
 //  To Do List
 //
-//  Created by Student on 1/11/22.
+//  Created by Dylan Koehler on 1/10/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toDoItems = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()), ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()), ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach (toDoItems) { item in
+                    Text(item.description)
+                    
+                }
+                .onMove(perform: { indices, newOffset in
+                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    toDoItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("To Do List", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +33,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
 }
