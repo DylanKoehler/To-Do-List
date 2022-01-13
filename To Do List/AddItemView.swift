1679802a8d1c7d265591d74bee8cbf2d6b734611
Dylan Koehler <Dylankoehler3212@gmail.com>
2022-01-13 10:39:10 -0600

    Created additional features to add items view.

---
+++ To Do List/AddItemView.swift
@@ -0,0 +1,44 @@
+//
+//  AddItemView.swift
+//  To Do List
+//
+//  Created by Student on 1/12/22.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let priorities = ["High", "Medium", "Low"]
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) { priority in
+                        Text(priority)
+                    }
+                }
+            }
+            .navigationBarTitle("Add New To Do Item", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Save") {
+                if priority.count > 0 && description.count > 0 {
+                    let item = ToDoItem(id: UUID(), priority: priority, description: description, dueDate: dueDate)
+                    toDoList.items.append(item)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList: ToDoList())
+    }
+}
